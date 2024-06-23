40ca6fbea4d1aed2608300bbc9bbb7cdd23cf3b8
Tejas M <97739480+playgms@users.noreply.github.com>
2024-06-23 23:14:18 +0000

    Update bin.scala

--- bin.scala
+++ bin.scala
@@ -1,1 +1,15 @@
+def binarySearch(arr: Array[Int], target: Int, low: Int = 0, high: Int = Int.MaxValue): Boolean = {
+  val h = if (high == Int.MaxValue) arr.length - 1 else high
+  if (low > h) false
+  else {
+    val mid = (low + h) / 2
+    if (arr(mid) == target) true
+    else if (arr(mid) > target) binarySearch(arr, target, low, mid - 1)
+    else binarySearch(arr, target, mid + 1, h)
+  }
+}
+
+val numbers = Array(1, 2, 3, 4, 5)
+println(binarySearch(numbers, 3))  // True
+println(binarySearch(numbers, 6))  // False
 
