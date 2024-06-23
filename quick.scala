78aad7d0e0907cc96a7415f5163eec353eec8b49
Tejas M <97739480+playgms@users.noreply.github.com>
2024-06-23 22:29:09 +0000

    Create quick.scala

---
+++ quick.scala
@@ -0,0 +1,11 @@
+def quickSort(arr: Array[Int]): Array[Int] = {
+  if (arr.length <= 1) arr
+  else {
+    val pivot = arr(arr.length / 2)
+    Array.concat(
+      quickSort(arr.filter(_ < pivot)),
+      arr.filter(_ == pivot),
+      quickSort(arr.filter(_ > pivot))
+    )
+  }
+}
